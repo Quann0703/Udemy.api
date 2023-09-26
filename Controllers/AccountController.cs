942354d21bff02e829d287b7fd28e89bb10cef52
Quann0703 <tranquan07112003@gmail.com>
2023-09-26 23:22:50 +0700

    a

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using BLL;
+using DataModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IAccountBLL _accountBll;
+        public AccountController(IAccountBLL accountBll)
+        {
+            _accountBll = accountBll;
+        }
+
+        [Route("get-by-id/{acountID}")]
+        [HttpGet]
+        public AccountModel GetDatabyID(string acountID)
+        {
+            return _accountBll.GetDatabyID(acountID);
+        }
+    }
+}
